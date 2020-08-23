388699942108fb5a34f967c0dc4896c05b7ff9ed
R-Imai <rimai.robo.cit@gmail.com>
2020-08-23 17:57:57 +0900

    ローカル以外からもつながるよう修正

--- client/src/Actions/ApiBase.ts
+++ client/src/Actions/ApiBase.ts
@@ -1,5 +1,6 @@
 export const API = {
-  UrlBase: "http://127.0.0.1:5555",
+  // UrlBase: "http://127.0.0.1:5555",
+  UrlBase: "http://192.168.1.20:5555",
   Auth: {
     login: "/api/login",
     logout: "/api/logout",
--- server/run.bat
+++ server/run.bat
@@ -1,2 +1,2 @@
 cd /d %~dp0
-uvicorn endpoint:app --reload --host 127.0.0.1 --port 5555
+uvicorn endpoint:app --reload --host 0.0.0.0 --port 5555
