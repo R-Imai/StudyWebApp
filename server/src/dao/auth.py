12287955917396adf5c46867d5f6043d03315975
R-Imai <rimai.robo.cit@gmail.com>
2020-09-06 17:33:50 +0900

    SQLパラメータ指定方法修正

--- server/src/dao/auth.py
+++ server/src/dao/auth.py
@@ -8,19 +8,19 @@ class AuthDAO:
     def __init__(self, connection):
         self.connection = connection
         self.query = {
-            "authentication": "SELECT user_id, create_date FROM active_token WHERE token='{0}'",
-            "login": "SELECT user_master.id, user_master.name, user_master.image, user_master.email FROM user_auth INNER JOIN user_master ON user_auth.id = user_master.id WHERE user_auth.id='{0}' AND user_auth.password='{1}'",
-            "create_token": "INSERT INTO active_token (token, user_id, create_date) VALUES ('{0}', '{1}', '{2}');",
-            "delete_token": "DELETE FROM active_token WHERE token = '{0}';",
-            "user_id_count":"SELECT count(*) as count FROM user_master WHERE id = '{0}';",
-            "register_user_master":"INSERT INTO user_master (id, name, image, email) values ('{0}', '{1}', '{2}', '{3}');",
-            "register_auth_info":"INSERT INTO user_auth (id, password) values ('{0}', '{1}');"
+            "authentication": "SELECT user_id, create_date FROM active_token WHERE token=%s",
+            "login": "SELECT user_master.id, user_master.name, user_master.image, user_master.email FROM user_auth INNER JOIN user_master ON user_auth.id = user_master.id WHERE user_auth.id=%s AND user_auth.password=%s",
+            "create_token": "INSERT INTO active_token (token, user_id, create_date) VALUES (%s, %s, %s);",
+            "delete_token": "DELETE FROM active_token WHERE token = %s;",
+            "user_id_count":"SELECT count(*) as count FROM user_master WHERE id = %s;",
+            "register_user_master":"INSERT INTO user_master (id, name, image, email) values (%s, %s, %s, %s);",
+            "register_auth_info":"INSERT INTO user_auth (id, password) values (%s, %s);"
         }
 
     def authentication(self, token):
         with self.connection.cursor() as cur:
-            query = self.query["authentication"].format(token)
-            cur.execute(query)
+            query = self.query["authentication"]
+            cur.execute(query, (token,))
             res = cur.fetchone()
         self.connection.commit()
         if res is None:
@@ -29,34 +29,34 @@ class AuthDAO:
 
     def login(self, id, password):
         with self.connection.cursor() as cur:
-            get_user_query = self.query["login"].format(id, password)
-            cur.execute(get_user_query)
+            get_user_query = self.query["login"]
+            cur.execute(get_user_query, (id, password))
             user_info = cur.fetchone()
             if user_info is None:
                 self.connection.commit()
                 return None
             token = str(uuid.uuid4())
             datetime_now = datetime.datetime.now()
-            set_token_query = self.query["create_token"].format(token, user_info[0], datetime_now)
-            cur.execute(set_token_query)
+            set_token_query = self.query["create_token"]
+            cur.execute(set_token_query, (token, user_info[0], datetime_now))
         self.connection.commit()
         return {"user_info": user_info, "token": token}
 
     def logout(self, token):
         with self.connection.cursor() as cur:
-            query = self.query["delete_token"].format(token)
-            cur.execute(query)
+            query = self.query["delete_token"]
+            cur.execute(query, (token,))
         self.connection.commit()
 
     def register(self, user_info):
         with self.connection.cursor() as cur:
-            user_id_count_query = self.query["user_id_count"].format(user_info.id)
-            cur.execute(user_id_count_query)
+            user_id_count_query = self.query["user_id_count"]
+            cur.execute(user_id_count_query, (user_info.id,))
             user_id_count = cur.fetchone()[0]
             if user_id_count > 0:
                 raise AlreadyExistExeption(f"ユーザID: '{user_info.id}'は既に使用されています。")
-            register_user_master_query = self.query["register_user_master"].format(user_info.id, user_info.name, user_info.image, user_info.email)
-            cur.execute(register_user_master_query)
-            register_auth_info_query = self.query["register_auth_info"].format(user_info.id, user_info.password);
-            cur.execute(register_auth_info_query)
+            register_user_master_query = self.query["register_user_master"]
+            cur.execute(register_user_master_query, (user_info.id, user_info.name, user_info.image, user_info.email))
+            register_auth_info_query = self.query["register_auth_info"]
+            cur.execute(register_auth_info_query, (user_info.id, user_info.password))
         self.connection.commit()
--- server/src/dao/user.py
+++ server/src/dao/user.py
@@ -4,13 +4,13 @@ class UserDAO:
     def __init__(self, connection):
         self.connection = connection
         self.query = {
-            "select_by_ueer_id": "SELECT id, name, image, email FROM user_master WHERE id='{0}'"
+            "select_by_ueer_id": "SELECT id, name, image, email FROM user_master WHERE id=%s"
         }
 
     def get_user_detail(self, user_id):
         with self.connection.cursor() as cur:
-            query = self.query["select_by_ueer_id"].format(user_id)
-            cur.execute(query)
+            query = self.query["select_by_ueer_id"]
+            cur.execute(query, (user_id,))
             res = cur.fetchone()
         self.connection.commit()
         return res
