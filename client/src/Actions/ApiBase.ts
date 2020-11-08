1970ede9d3fd1fe96780e4d4fab372c28c9597a1
R-Imai <rimai.robo.cit@gmail.com>
2020-11-08 14:40:50 +0900

    webサーバー用の設定追加

--- client/src/Actions/ApiBase.ts
+++ client/src/Actions/ApiBase.ts
@@ -1,6 +1,7 @@
 export const API = {
   // UrlBase: "http://127.0.0.1:5555",
-  UrlBase: "http://192.168.1.20:5555",
+  // UrlBase: "http://192.168.1.20:5555",
+  UrlBase: "",
   Auth: {
     login: "/api/login",
     logout: "/api/logout",
